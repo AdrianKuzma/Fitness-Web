3cbd923dc61d871b4523c5daeec7a3a10a2b0291
Adrian Kuzma <beaversvk@gmail.com>
2021-10-13 19:32:29 +0200

    Added back and front

---
+++ fitness-back/src/main/java/sk/fitness/fitnessweb/FitnessWebApplication.java
@@ -0,0 +1,13 @@
+package sk.fitness.fitnessweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessWebApplication.class, args);
+	}
+
+}
---
+++ fitness-back/src/test/java/sk/fitness/fitnessweb/FitnessWebApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.fitness.fitnessweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
