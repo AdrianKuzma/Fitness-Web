c62ce70ef2cba22bcb7cea97bfb774f157e48126
luboslavistenik <luboslav.istenik@gmail.com>
2021-11-03 21:42:46 +0100

    exercises

--- fitness-fronted/src/app/app-routing.module.ts
+++ fitness-fronted/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { ExercisesComponent } from './exercises/exercises.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomePageComponent} from './home-page/home-page.component'
+import { HomePageComponent} from './home-page/home-page.component'
+import { TrainingComponent } from './training/training.component';
+import { SupplementsComponent } from './supplements/supplements.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomePageComponent},
+  { path: 'exercises', component: ExercisesComponent},
+  { path: 'training', component: TrainingComponent},
+  { path: 'supplements', component: SupplementsComponent},
   { path: '**', redirectTo: 'home' },
-  { path: 'home', component: HomePageComponent}
 ];
 
 @NgModule({
--- fitness-fronted/src/app/app.module.ts
+++ fitness-fronted/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { ExercisesComponent } from './exercises/exercises.component';
+import { TrainingComponent } from './training/training.component';
+import { SupplementsComponent } from './supplements/supplements.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    ExercisesComponent,
+    TrainingComponent,
+    SupplementsComponent
   ],
   imports: [
     BrowserModule,
